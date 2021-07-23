bc276150bb1fab0ac12e7f5c952200e6754b0bd1
yunju-jang <yjjang305@naver.com>
2021-07-23 22:34:51 +0900

    210723

--- main.js
+++ main.js
@@ -14,9 +14,22 @@ var app = express();
 const $url = 'http://211.237.50.150:7080/openapi/sample/xml/Grid_20150827000000000226_1/1/5'
 const $KEY = 'e8e79f2dfe0db907b387019f97ac979a9bba1d7e30cc349e37afd92326faefbf'
 const $station = '195453'
+const $sample = 'http://211.237.50.150:7080/openapi/e8e79f2dfe0db907b387019f97ac979a9bba1d7e30cc349e37afd92326faefbf/xml/Grid_20150827000000000226_1/20/25'
 
 app.listen(3000, 'localhost', function () {
     console.log('서버 실행 중...');
+});
+
+console.log($sample);
+
+request($sample, function(err, res, body) {
+    $ = cheerio.load(body);
+    
+    $('row').each(function(idx) {
+        let no1 = $(this).find('RECIPE_NM_KO').text();
+        let no2 = $(this).find('SUMRY').text();
+        console.log(`레시피이름 : ${no1}, 소개 : ${no2}`);
+    });
 });
 
 app.use(bodyParser.urlencoded({extended: false}));
@@ -25,10 +38,16 @@ app.get('/', (req, res) => {
     res.send({"result": "GET 호출"});
 })
 
+app.post('/mealkit', function(req, res) {
+    var recipe_name = req.body.recipe_name;
+});
+
 app.post('/user/join', function (req, res) {
     var userEmail = req.body.userEmail;
     var userPwd = req.body.userPwd;
     var userName = req.body.userName;
+    
+    console.log(userEmail,userPwd, userName);
 
     // 삽입을 수행하는 sql문.
     var sql = 'INSERT INTO Users (UserEmail, UserPwd, UserName) VALUES (?, ?, ?)';
